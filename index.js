edb3f6807f680efd1eda23d8f03e76675619e6d8
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-23 22:18:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ function BoardMember(name, homeState, training){
   this.training = training,
   this.veto = function(){
     return "No, I must disagree";
-  }
+  };
   this.approve = function(){
     return "You can do that!";
-  }
+  };
   this.doCharity = function(){
     "I like to help people";
-  }
+  };
+  this.releasePressStatement = function(){
+    return "You will see great things from Scuber.";
+  };
+  this.sayHi = function(){
+    returns `Hi, my name is ${this.name}.  I am from ${this.homeState}, and I was trained in ${this.training}.`;
+  }; 
 }
