4d3df0ab8777000711f6c52965bba433b528d23f
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-23 22:13:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function BoardMember(name, homeState, training){
+  this.name = name,
+  this.homeState = homeState,
+  this.training = training,
+  function veto(){
+    return "No, I must disagree"
+  }
+}
